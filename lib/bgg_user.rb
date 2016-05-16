63c1165fd1cd33feb06f3970839ee0fd4b40676d
May Roussel <may@maybreak.com>
2016-05-16 18:55:11 -0500

    bgg user nulling

--- lib/bgg_user.rb
+++ lib/bgg_user.rb
@@ -4,13 +4,22 @@ module BoardGameGem
 		attr_reader :id, :name, :avatar, :year_registered, :last_login, :state, :trade_rating
 
 		def initialize(xml)
-			@id = get_integer(xml, "user", "id")
-			@name = get_string(xml, "user", "name")
-			@avatar = get_string(xml, "avatarlink", "value")
-			@year_registered = get_integer(xml, "yearregistered", "value")
-			@last_login = get_string(xml, "lastlogin", "value")
-			@state = get_string(xml, "stateorprovince", "value")
-			@trade_rating = get_integer(xml, "traderating", "value")
+			if !xml.nil?
+				@id = get_integer(xml, "user", "id")
+				@name = get_string(xml, "user", "name")
+				@avatar = get_string(xml, "avatarlink", "value")
+				@year_registered = get_integer(xml, "yearregistered", "value")
+				@last_login = get_string(xml, "lastlogin", "value")
+				@state = get_string(xml, "stateorprovince", "value")
+				@trade_rating = get_integer(xml, "traderating", "value")
+			else
+				@id = -1
+				@name = ""
+				@avatar = ""
+				@year_registered = -1
+				@last_login = "0000-00-00 00:00:00"
+				@state = ""
+				@trade_rating = -1
 		end
 
 		def get_collection
