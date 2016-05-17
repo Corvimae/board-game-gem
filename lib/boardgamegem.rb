f0b5bc9688815abcc17aa3a104a34b41ed261c71
Jake Roussel <jakeroussel@mac.com>
2016-05-16 21:10:45 -0500

    module_function

--- lib/boardgamegem.rb
+++ lib/boardgamegem.rb
@@ -5,14 +5,15 @@ module BoardGameGem
 	API_ROOT = "https://www.boardgamegeek.com/xmlapi2"
 	MAX_ATTEMPTS = 10
 
-	def BoardGameGem.get_item(id, statistics = false, options = {})
+	module_function
+	def get_item(id, statistics = false, options = {})
 		options[:id] = id
 		options[:stats] = statistics ? 1 : 0
 		item = BGGItem.new(BoardGameGem.request_xml("thing", options))
 		return item.id == 0 ? nil : item
 	end
 
-	def BoardGameGem.get_items(ids, statistics = false, options = {})
+	def get_items(ids, statistics = false, options = {})
 		options[:id] = ids.join(",")
 		options[:stats] = statistics ? 1 : 0
 		item_xml = BoardGameGem.request_xml("thing", options)
@@ -24,19 +25,19 @@ module BoardGameGem
 		item_list
 	end
 
-	def BoardGameGem.get_family(id, options = {})
+	def get_family(id, options = {})
 		options[:id] = id
 		family = BGGFamily.new(BoardGameGem.request_xml("family", options))
 		return family.id == 0 ? nil : family
 	end
 
-	def BoardGameGem.get_user(username, options = {})
+	def get_user(username, options = {})
 		options[:name] = username
 		user = BGGUser.new(BoardGameGem.request_xml("user", options))
 		return user.id == 0 ? nil : user
 	end
 
-	def BoardGameGem.get_collection(username, options = {})
+	def get_collection(username, options = {})
 		options[:username] = username
 		collection_xml = BoardGameGem.request_xml("collection", options)
 		if collection_xml.css("error").length > 0
@@ -46,7 +47,7 @@ module BoardGameGem
 		end
 	end
 
-	def BoardGameGem.search(query, options = {})
+	def search(query, options = {})
 		options[:query] = query
 		xml = BoardGameGem.request_xml("search", options)
 		return {
