063a119139ffef2afa153e14353df24e74f6106f
May Roussel <may@maybreak.com>
2016-05-24 21:15:35 -0500

    collection attr_accessor

--- test.rb
+++ test.rb
@@ -1,8 +1,2 @@
 require_relative 'lib/board-game-gem'
-#p BoardGameGem.get_items([15944, 15955, 15956]).to_s
-
-p BoardGameGem.get_items([123260, 123265, 61001], true).map { |x| x.type }
-
-
-p BoardGameGem.get_item(123260, true)
-
+p BoardGameGem.get_collection("sos1", subtype: "boardgame,videogame,rpgitem").items.select { |x| x.type == "videogame" }
